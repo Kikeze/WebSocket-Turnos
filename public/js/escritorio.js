4c301544d47b2eecc2990243ee66d2c9c3b62df1
Huntress <quiqueze@gmail.com>
2022-09-03 20:09:40 -0500

    Se agrega la opcion para atender Ticket

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,60 @@
+const lblEscritorio = document.querySelector("h1");
+const btnAtender = document.querySelector("button");
+const lblTicket = document.querySelector("small");
+const divAlert = document.querySelector(".alert");
+
+const search = new URLSearchParams( window.location.search );
+
+if( !search.has("escritorio") ) {
+    window.location = "index.html";
+    throw new Error("El escritorio es obligatorio");
+}
+
+const escritorio = search.get("escritorio");
+lblEscritorio.innerText = escritorio;
+divAlert.style.display = "none";
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    // lblNuevoTicket.innerText = `Ticket ${ ultimo }`;
+});
+
+btnAtender.addEventListener("click", (e) => {
+    socket.emit("atender-ticket", {escritorio}, (payload) => {
+        if( !payload.ok ) {
+            lblTicket.innerText = "Nadie";
+            divAlert.innerText = payload.msg;
+            divAlert.style.display = "";
+            return;
+        }
+
+        lblTicket.innerText = `Ticket ${payload.ticket.numero}`;
+    });
+});
 
 
-console.log('Escritorio HTML');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sockets/controller.js
+++ sockets/controller.js
@@ -7,11 +7,36 @@ const socketController = (socket) => {
 
     socket.emit("ultimo-ticket", ticketControl.ultimo);
     
-    socket.on('siguiente-ticket', ( payload, callback ) => {
+    socket.on("siguiente-ticket", (payload, callback) => {
         const siguiente = ticketControl.siguiente();
         callback( siguiente );
 
         //TODO: Notificar nuevo ticket
+    });
+
+    socket.on("atender-ticket", (payload, callback) => {
+        const escritorio = payload.escritorio;
+
+        if( !escritorio ) {
+            return callback({
+                ok: false,
+                msg: "No se especifico el escritorio"
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket( escritorio );
+        if( !ticket ) {
+            return callback({
+                ok: false,
+                msg: "No hay tickets por atender"
+            });
+        }
+        else {
+            return callback({
+                ok: true,
+                ticket
+            });
+        }
     });
 
 };
